e2eb9bb498a30aac395c1c84f35897968da814ec
KatarzynaWatroba <64368089+KatarzynaWatroba@users.noreply.github.com>
2020-05-11 18:08:18 +0000

    Add files via upload

---
+++ sekretnaliczba.py
@@ -0,0 +1,15 @@
+polecenie = input("wprowadz liczbe calkowita: ")
+while tajnyNumer == 777:
+    print("Dobra robota")
+ #tajnyNumer = 777
+
+
+poczatek = input("wprowadz liczbe calkowita: "
+
+
+while tajnyNumer and polecenie == 777:
+	print("to jest ta tajna liczba")
+if "tajnyNumer" > 777 or "tajnyNumer" < 777:
+	print("ojejku probuj dalej")
+else:
+	print(input("sprobuj ponownie")
